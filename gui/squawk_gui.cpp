d53b057c878f916d5ca72417204420d5fdd35110
etienne <squawkcpp@gmail.com>
2017-06-09 11:20:04 +0000

    add gui subproject

---
+++ gui/squawk_gui.cpp
@@ -0,0 +1,14 @@
+#include "squawk_gui.h"
+
+#include <iostream>
+
+squawk_gui::squawk_gui()
+{
+
+}
+
+
+int main(int argc, char **argv) {
+    std::cout << "Run squawk gui" << std::endl;
+    return 0;
+}
---
+++ gui/squawk_gui.h
@@ -0,0 +1,11 @@
+#ifndef SQUAWK_GUI_H
+#define SQUAWK_GUI_H
+
+
+class squawk_gui
+{
+public:
+    squawk_gui();
+};
+
+#endif // SQUAWK_GUI_H
